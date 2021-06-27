edb83075f02220555621f0f80f9743a47c8abc4c
wolven531 <anthony.williams.cs@gmail.com>
2021-06-27 17:43:06 -0600

    Add basic App logic / layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid, Typography } from '@material-ui/core'
+import React, { FC } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import './App.css'
+import { Home } from './Home'
+import { Nav } from './Nav'
+import { OtherPage } from './OtherPage'
+import { TitleContextProvider } from './TitleContextProvider'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: FC = () => {
+	return (
+		<TitleContextProvider>
+			<Grid container>
+				<Grid item xs={12}>
+					<Nav />
+				</Grid>
+				<Grid item container direction="column" xs={12}>
+					<Grid item xs={2} />
+					<Grid item xs={8}>
+						<BrowserRouter>
+							<Switch>
+								<Route path="/" exact>
+									<Home />
+								</Route>
+								<Route path="/other-page">
+									<OtherPage />
+								</Route>
+							</Switch>
+						</BrowserRouter>
+					</Grid>
+					<Grid item xs={2} />
+				</Grid>
+				<Grid item xs={12}>
+					<Typography>Footer</Typography>
+				</Grid>
+			</Grid>
+		</TitleContextProvider>
+	)
 }
 
-export default App;
+export { App }
