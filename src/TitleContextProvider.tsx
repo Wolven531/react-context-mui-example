d0dae2e4502671bdc37ea75ecc2daa80b97830f0
wolven531 <anthony.williams.cs@gmail.com>
2021-06-27 22:25:45 -0600

    Clarify some types

--- src/TitleContextProvider.tsx
+++ src/TitleContextProvider.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react'
+import React, { createContext, FC, useState } from 'react'
 
-const { Provider, Consumer } = React.createContext({
+const { Provider, Consumer } = createContext({
 	setTitle: (newTitle: string) => {
 		alert('using default function to set title, WONT WORK')
 	},
 	title: 'Default Title From Creation',
 })
 
-const TitleContextProvider = (props: any) => {
+const TitleContextProvider: FC<any> = (props: any) => {
 	const [title, setTitle] = useState('Default Title From useState')
 
 	return <Provider value={{ title, setTitle }}>{props.children}</Provider>
